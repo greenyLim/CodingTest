8f5bf57ac90c80f399768aef736f261afc7a4789
ChaeHyun Lim <136687133+greenyLim@users.noreply.github.com>
2025-03-27 22:33:11 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1439. 뒤집기/뒤집기.py
+++ Python/백준/Silver/1439. 뒤집기/뒤집기.py
@@ -4,10 +4,6 @@ input= sys.stdin.readline
 lst= list(input().strip())
 
 split=[]
-a=lst.count('0')
-b=lst.count('1')
-
-
 split.append([lst[0],lst[0]])
 
 for i in lst[1:]:
@@ -16,10 +12,7 @@ for i in lst[1:]:
         split[-1][1]=split[-1][1]+i
     else:
         split.append([i,i])
-
-
-
-#split.sort(key=lambda x:len(x[1]))
+    #print(split)
 
 zero=0
 one=0
@@ -30,15 +23,4 @@ for i in split:
     if i[0]=='1':
         one+=1
 
-if zero>one:
-    answer=one
-else:
-    answer=zero
-if zero==0 or one==0:
-    answer=0
-
-
-
-
-
-print(answer)
+print(min(zero, one))
