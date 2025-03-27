60a7c3e72500301e3ad52e0cc3a5aacdceb9e05c
ChaeHyun Lim <136687133+greenyLim@users.noreply.github.com>
2025-03-27 23:24:22 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ MySQL/프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요
-SELECT I.NAME, I.DATETIME
-FROM ANIMAL_INS I
-LEFT JOIN ANIMAL_OUTS O
-ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE O.DATETIME IS NULL
-ORDER BY DATETIME 
-LIMIT 3
+select i.name, i.datetime
+from animal_ins i 
+left join animal_outs o on i.animal_id=o.animal_id
+where i.animal_id not in (select o.animal_id
+                          from animal_outs o)
+order by datetime
+limit 3
