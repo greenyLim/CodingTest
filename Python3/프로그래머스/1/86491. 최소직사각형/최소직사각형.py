bd6531cd6a95652d63a07b3fcb66159edca0b50d
ChaeHyun Lim <136687133+greenyLim@users.noreply.github.com>
2026-01-18 22:18:17 +0900

    [level 1] Title: 최소직사각형, Time: 2.74 ms, Memory: 10.5 MB -BaekjoonHub

--- Python3/프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ Python3/프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,15 @@
+import sys
+input = sys.stdin.readline
+
 def solution(sizes):
-    long=[]
-    short=[]
-    for size in sizes:
-        long.append(max(size))
-        short.append(min(size))  
-    return max(long)*max(short)
+    weight =[]
+    height =[]
+    
+    for s in sizes:
+        s.sort(reverse=True)
+        weight.append(s[0])
+        height.append(s[1])
+        #print(max(weight))
+    answer = max(weight)*max(height)
+    return answer
+    
